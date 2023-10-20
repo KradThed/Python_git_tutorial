3b13133aca2181bd7a316aca3175460e71a2cc65
asura <capxleb@gmail.com>
2023-10-21 01:31:00 +0300

    add min temperature and dates

--- weather_hystogram.py
+++ weather_hystogram.py
@@ -1,7 +1,8 @@
 import csv
 from matplotlib import pyplot as plt
+from datetime import datetime
 
-filename = 'D:\WORK\Learning\csv\weather_data\sitka_weather_07-2021_simple.csv'
+filename = 'D:\WORK\Learning\csv\weather_data\sitka_weather_2021_simple.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
@@ -10,19 +11,28 @@ with open(filename) as f:
 
     # Чтение максимальных температура 
     highs = []
+    dates = []
+    lows = []
     for row in reader:
-        high = int(row[5])
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        dates.append(current_date)
+        high = int(row[4])
+        low = int(row[5])
         highs.append(high)
+        lows.append(low)
 
     print(highs)
 
 # нанесение данных на гистограмму
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='blue')
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Форматирование диаграммы
-plt.title("Daily hight temperatures, 07/2021", fontsize=24)
+plt.title("Daily high and low temperatures - 2021", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both',which='major', labelsize=16)
 
